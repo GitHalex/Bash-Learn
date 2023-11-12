60a26ce663aa1f0f6f13d7f60f88b12fac44f5d8
GitHalex <calex1731@gmail.com>
2023-11-12 12:15:34 -0400

    file getname finished

---
+++ getname.sh
@@ -0,0 +1,6 @@
+#! /bin//bash
+# Interactive reading of a variable
+echo "ESCRIBE TU NOMBRE"
+read nombre
+#Display variable input
+echo El nombre escrito es: $nombre
--- hello.sh
+++ hello.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-echo "Hello Linux Foundation Student"
+echo "Hello Linux Foundation Studentls "
 
