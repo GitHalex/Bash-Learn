86652f8c60212438173c4ca582684eea9f4090c4
GitHalex <calex1731@gmail.com>
2024-02-20 15:52:25 -0400

    segundo Script terminado

--- Project/countdown.sh
+++ Project/countdown.sh
@@ -2,18 +2,31 @@
 
 # Program that counts down to zero from a given argument
 
-# if [[ $1 == arg1 ]]
-# then 
-#    echo true
-# else
-#    echo false
-# fi
+:   '
+if [[ $1 == arg1 ]]
+then 
+   echo true
+else
+   echo false
+fi
+'
+
+echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1  -gt 0  ]]
 then
-   for (( i = $1; i > 0; i--))
+   : 'for (( i = $1; i >= 0; i--))
    do
+      sleep 3
       echo $i
+   done
+   '
+   I=$1
+   while [[ $I -ge 0 ]]
+   do
+      echo $I
+      (( I-- ))
+      sleep 1
    done
 else
    echo Include a positive integer as the first argument.
