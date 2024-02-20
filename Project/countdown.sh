ef71c3b87a5577fbb2a94c98725d4a2cc622db37
GitHalex <calex1731@gmail.com>
2024-02-20 14:20:02 -0400

    Agregando un ciclo for al script

--- Project/countdown.sh
+++ Project/countdown.sh
@@ -2,9 +2,19 @@
 
 # Program that counts down to zero from a given argument
 
-if [[ $1 == arg1 ]]
-then 
-   echo true
+# if [[ $1 == arg1 ]]
+# then 
+#    echo true
+# else
+#    echo false
+# fi
+
+if [[ $1  -gt 0  ]]
+then
+   for (( i = $1; i > 0; i--))
+   do
+      echo $i
+   done
 else
-   echo false
+   echo Include a positive integer as the first argument.
 fi
